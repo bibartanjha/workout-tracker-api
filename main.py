e5c1c4dd3f3a41488a739e6e05f68a59cffc7829
Bibartan Jha <jha.b1bartan@utexas.edu>
2025-09-29 22:02:24 -0500

    added get_categories

--- main.py
+++ main.py
@@ -65,3 +65,11 @@ def get_workouts():
         result = connection.execute(text("select * FROM workout_records ORDER BY date DESC"))
         rows = [Workout(**row._mapping) for row in result]
     return [workout.valueswithlabels() for workout in rows]
+
+@app.get("/categories")
+def get_categories():
+    query = "SELECT DISTINCT category FROM exercise_categories"
+    with engine.connect() as connection:
+        result = connection.execute(text(query))
+        categories = [row[0] for row in result.fetchall()]
+    return categories
